d87000a889478a3e8d8379bca375f3b181b0516f
German Ho <germanho112@gmail.com>
2023-01-22 20:56:51 +0800

    feat

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ac700fa679adf439394a26981a11a34a-45802461.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
